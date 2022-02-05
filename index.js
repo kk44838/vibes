8c814fc3d38a44bbb607afd991b68f6cab0a96f9
Alex Liu <alexliu@dyn3139-160.wlan.ic.ac.uk>
2022-02-05 15:31:50 +0000

    Add dynamic commands

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+// // Require the necessary discord.js classes
+// const { Client, Intents } = require('discord.js');
+// const { token } = require('./config.json');
+
+// // Create a new client instance
+// const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// // // When the client is ready, run this code (only once)
+// client.once('ready', () => {
+// 	console.log('Ready!');
+// });
+
+// client.on('interactionCreate', async interaction => {
+// 	if (!interaction.isCommand()) return;
+
+// 	const { commandName } = interaction;
+
+// 	if (commandName === 'ping') {
+// 		await interaction.reply('Kas is gay');
+// 	} else if (commandName === 'server') {
+// 		await interaction.reply('Server info.');
+// 	} else if (commandName === 'user') {
+// 		await interaction.reply('User info.');
+// 	} else if (commandName === 'servername') {
+// 		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+// 	}
+// });
+
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 // Login to Discord with your client's token
