8611b0fe0602cebf51cd96bbe27c958f0787eef0
Alex Liu <alexliu@dyn3139-160.wlan.ic.ac.uk>
2022-02-05 23:46:35 +0000

    Implement shuffle

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,6 +1,16 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const music = require("../music.js");
 
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
 function songsToString(songs) {
     const queueShowLength = 10
     const slicedArray = songs.slice(0, queueShowLength);
@@ -16,11 +26,19 @@ function songsToString(songs) {
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('queue')
-		.setDescription('Queue'),
+		.setName('shuffle')
+		.setDescription('Shuffle the queue'),
 	async execute(interaction) {
         const serverQueue = music.queue.get(interaction.guild.id);
         // console.log(serverQueue.songs)
+
+        const head = serverQueue.songs[0];
+
+        serverQueue.songs.shift();
+
+        shuffleArray(serverQueue.songs);
+
+        serverQueue.songs.unshift(head);
 
 		await interaction.reply(songsToString(serverQueue.songs))
 		
