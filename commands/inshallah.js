59fb238466388c63a3c40548a2676aa3febe24cf
kk44838 <kazuya.kaiolowu@gmail.com>
2022-02-05 22:19:19 +0000

    inshallah

---
+++ commands/inshallah.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const music = require("../music.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('inshallah')
+		.setDescription('Siuuu'),
+	async execute(interaction) {
+        const songName = "Inshallah Siuu"
+        const serverQueue = music.queue.get(interaction.guild.id);
+
+        await music.play(interaction, "IX51UAJUhhQ", serverQueue);
+	},
+};
