d7d31fc88908b3880874cc135c062966f66958a5
kk44838 <kazuya.kaiolowu@gmail.com>
2022-02-05 23:23:57 +0000

    Queue works

--- commands/queue.js
+++ commands/queue.js
@@ -1,14 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const music = require("../music.js");
 
+function songsToString(songs) {
+    const queueShowLength = 10
+    const slicedArray = songs.slice(0, queueShowLength);
+    console.log(`songsToString ${slicedArray}`)
+    let msg = 'Songs in Queue:' + '\n';
+    slicedArray.forEach(song => {
+        msg += song.title + '\n';
+    });
+    console.log(msg)
+    return msg
+}
+
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('queue')
 		.setDescription('Queue'),
 	async execute(interaction) {
         const serverQueue = music.queue.get(interaction.guild.id);
-        console.log(serverQueue.songs)
-		await interaction.reply("queue")
+        // console.log(serverQueue.songs)
+
+		await interaction.reply(songsToString(serverQueue.songs))
 		
 	},
 };
